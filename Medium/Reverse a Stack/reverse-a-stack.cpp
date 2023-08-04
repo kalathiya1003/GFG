a89097b0de5f18918105cb3b7ee6739fcb01fb80
CSE_BE_26_203258111 <139961986+kalathiya1003@users.noreply.github.com>
2023-08-04 14:23:13 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Reverse a Stack/reverse-a-stack.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+   
+   void solve(stack<int>&st,stack<int>&temp){
+       if(st.empty())return;
+       
+       int num= st.top();
+       temp.push(num);
+       st.pop();
+       solve(st,temp);
+   }
+    
+    void Reverse(stack<int> &St){
+        stack<int>temp;
+        solve(St,temp);
+        St=temp;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int N;
+        cin>>N;
+        stack<int> St;
+        for(int i=0;i<N;i++){
+            int x;
+            cin>>x;
+            St.push(x);
+        }
+        Solution ob;
+        ob.Reverse(St);
+        vector<int>res;
+        while(St.size())
+        {
+            res.push_back(St.top());
+            St.pop();
+        }
+        for(int i = res.size()-1;i>=0;i--)
+        {
+            cout<<res[i]<<" ";
+        }
+        
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
