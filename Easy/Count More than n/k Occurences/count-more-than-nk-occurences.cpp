9b30467ee6035ab018e25f8f76313661b943597d
CSE_BE_26_203258111 <139961986+kalathiya1003@users.noreply.github.com>
2023-12-23 09:45:34 -0800

    Updated Solution - GfG to GitHub

--- Easy/Count More than n/k Occurences/count-more-than-nk-occurences.cpp
+++ Easy/Count More than n/k Occurences/count-more-than-nk-occurences.cpp
@@ -10,7 +10,7 @@ class Solution
 {
     public:
     //Function to find all elements in array that appear more than n/k times.
- int countOccurence(int arr[], int n, int k) {
+   int countOccurence(int arr[], int n, int k) {
        int ans=0;
        unordered_map<int,int> mp;
        for(int i=0;i<n;i++) mp[arr[i]]++;
